8cbc37809e3b605888a93898ac5e697db279ad3a
mjsgit870 <saputraj870@gmail.com>
2021-08-22 00:58:48 +0700

    Perbaikan sidebar

--- pages/component/layout.js
+++ pages/component/layout.js
@@ -3,7 +3,7 @@ import Sidebar from "./sidebar";
 
 export default function Layout({ children }) {
   return (
-    <div className="md:px-20 py-2 px-1 md:px-0 grid md:grid-cols-3 gap-2">
+    <div className="md:px-20 py-2 px-1 grid md:grid-cols-3 gap-2">
       <Sidebar />
       <div className="secondary md:col-span-2 py-2 px-3 radius">
         <div className="mb-5">
